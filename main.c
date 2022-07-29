b34e59d511a236fcbb386783cc18a6d950e5b4b2
Alajandro <alejandroo@alejandria.com>
2022-07-29 15:10:00 -0500

    add exclamation marks

--- main.c
+++ main.c
@@ -2,14 +2,6 @@
 #include <stdlib.h>
 
 int main(){
-    printf("La vida es bella\n");
+    printf("La vida es bella!!!\n");
     return(EXIT_SUCCESS);
 }
-
-
-asd
-__attribute_used__asd
-asd
-asmdas
-daddr_tasd
-sadsa
